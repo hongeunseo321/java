8792c5ca496f61bbc524e310f1bbb0a391f45ab7
hongeunseo321 <hhyyss5520@gmail.com>
2025-07-29 17:52:29 +0900

    111

--- Chapter04_제어문(반복문,제어문문제)/src/문제7.java
+++ Chapter04_제어문(반복문,제어문문제)/src/문제7.java
@@ -10,6 +10,24 @@ public class 문제7 {
 				continue; // i++ => 증가식으로 올라간다
 			System.out.print(i+" ");
 		}
+		
+//		String[] arr= {"aaa","bbb","ccc","ddd","eee","fff","iii","jjj","ggg"};
+//		System.out.println(arr[0]);
+//		System.out.println(arr[1]);
+//		System.out.println(arr[2]);
+//		
+//		System.out.println(arr[3]);
+//		System.out.println(arr[4]);
+//		System.out.println(arr[5]);
+//		
+//		System.out.println(arr[6]);
+//		System.out.println(arr[7]);
+//		System.out.println(arr[8]);
+//		
+//		for(int i=0;i<9;i++)
+//		{
+//			System.out.println(arr[i]);
+//		}
 	}
 
 }
