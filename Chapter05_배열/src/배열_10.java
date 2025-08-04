c53b30792a1442fbce9f33e26c49a5d05bc24170
hongeunseo321 <hhyyss5520@gmail.com>
2025-08-04 14:02:00 +0900

    111

---
+++ Chapter05_배열/src/배열_10.java
@@ -0,0 +1,40 @@
+// 예약일
+public class 배열_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] lotto=new int[6];
+		int su=0; // 난수
+		boolean bCheck=false;
+		
+		for(int i=0;i<lotto.length;i++)
+		{
+			bCheck=true;
+			while(bCheck) // 중복 횟수
+			{
+				su=(int)(Math.random()*45)+1;
+				bCheck=false;
+				// 중복
+				for(int j=0;j<i;j++)
+				{
+					if(lotto[i]==lotto[j]) // 중복이면 while문으로 이동
+					{
+						bCheck=true;
+						break;
+					}
+//					else
+//					{
+//						bCheck=false;
+//						break;
+//					} => 넣으면 안 되는 문장
+				}
+			}
+			lotto[i]=su;
+		}
+		for(int i:lotto)
+		{
+			System.out.print(i+" ");
+		}
+	}
+
+}
---
+++ Chapter05_배열/src/배열_11.java
@@ -0,0 +1,11 @@
+// => 데이터형 설정 => 몇 개 저장 => 초기화
+// 임의로 저장 / 기본값 (new)
+// 데이터가 있는 경우에는 임의로 저장 / 입력값을 받을 때는 기본값 (new)
+public class 배열_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
