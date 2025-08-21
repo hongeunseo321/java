2f26e0a200ebe74311e163f2abdab10ae577c599
hongeunseo321 <hhyyss5520@gmail.com>
2025-08-21 17:51:26 +0900

    111

---
+++ Chapter12_라이브러리(java.lang)/src/com/sist/lang/라이브러리_9.java
@@ -0,0 +1,73 @@
+package com.sist.lang;
+import java.text.*;
+import java.util.Date;
+public class 라이브러리_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int won=1000000000;
+//		DecimalFormat df=new DecimalFormat("#,###,###,###");
+//		System.out.println(df.format(won));
+		Date date=new Date();
+		System.out.println(date);
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		/*
+		 * yyyy / yy
+		 * MM   / M => 대문자 (소문자는 분)
+		 * dd   / d
+		 * ---------
+		 * hh   / h
+		 * mm   / m
+		 * ss   / s
+		 */
+		System.out.println(sdf.format(date));
+		int no=1;
+		String name="홍길동";
+		String sex="남자";
+		String address="서울";
+		String phone="010-1111-1111";
+		int age=30;
+		String content="홍길동입니다";
+		
+		// 오라클 연동
+		String sql="INSERT INTO member Values("
+				+no+",'"+name+"','"+sex+"','"
+				+address+"','"+phone+"',"+age+")";	
+		System.out.println(sql);
+		
+		sql="INSERT INTO member VALUES("
+				+"{0},''{1}'',''{2}'',''{3}'',''{4}'',{5})";
+		Object[] obj= {no, name, sex, address, phone, age};
+		System.out.println(MessageFormat.format(sql, obj));
+		
+		double[] limit= {59, 60, 70, 80, 90};
+		// 0~59, 60~69, 70~79, 80~89, 90이상
+		String[] grade= {"F","D","C","B","A"};
+		int[] score=new int[10];
+		for(int i=0;i<score.length;i++)
+		{
+			score[i]=(int)(Math.random()*101);
+		};
+		
+		ChoiceFormat cf=new ChoiceFormat(limit,grade);
+		for(int i=0;i<score.length;i++)
+		{
+			System.out.println(score[i]+":"
+					+cf.format(score[i]));
+		}
+		
+		/*
+		 * 오라클 => 문자 함수 / 숫자 함수 / 날짜 함수 / 변환 함수 / 기타 함수
+		 *            |         |         |         |        |
+		 *          String     Math    Calendar   Format   switch, if
+		 *          
+		 * SimpleDateFormat : 날짜 변경 => 날짜 함수 (오라클)
+		 * DecimalFormat : 숫자 변환 => 숫자 함수 (오라클)
+		 * MessageFormat : SQL문장 사용
+		 * --------------------------
+		 * => 웹 : SQL (BackEnd : 90%)
+		 * -------------------------- 
+		 */
+	}
+
+}
---
+++ Chapter12_라이브러리(java.util)/src/com/sist/util/라이브러리_StringTokenizer_1.java
@@ -0,0 +1,58 @@
+package com.sist.util;
+/*
+ * util : 유용한 클래스의 집합
+ * 	1) StringTokenizer : 문자 분해***
+ * 	2) Collection***
+ * 		List형 => 순서를 가지고 있다(인덱스), 데이터 중복 허용
+ *		 => 데이터베이스(비동기)
+ *              |        네트워크의 접속자 저장(동기)
+ *              |        |                   |
+ * 			ArrayList, Vector, LinkedList, Queue
+ * 
+ * 		Set형 => 순서가 없다, 데이터 중복 허용 X
+ * 			HashSet, TreeSet
+ * 
+ * 		Map형 => 순서가 없다, 쌍으로 이루어져 있다 (키,값)
+ * 			HashMap (동기), Hashtable (비동기)
+ * 	------------------------------------------------ 핵심
+ * 	3) Data / Calendar : 날짜 관련 
+ * 	
+ * 
+ * StringTokenizer : 구분자별로 문자열을 나눠주는 역할
+ *  1) 생성자 => 구분
+ *  2) countTokens() : 분리한 단어의 개수
+ *  3) hasMoreTokens : 루프 사용 시 (반복문)
+ *      => 분리된 단어가 몇 개인지 모르는 경우
+ *      => 개수가 명확하지 않은 경우
+ *  4) nextToken : 분리된 단어를 읽어올 경우 
+ *       
+ *       
+ */
+import java.util.*;
+public class 라이브러리_StringTokenizer_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String colors="red,green,blue,black,yellow,pink,magenta";
+		StringTokenizer st=new StringTokenizer(colors,",");
+		System.out.println("Color 개수:"+st.countTokens());
+//		System.out.println("1' 색상:"+st.nextToken());
+//		System.out.println("2' 색상:"+st.nextToken());
+//		System.out.println("3' 색상:"+st.nextToken());
+//		System.out.println("4' 색상:"+st.nextToken());
+//		System.out.println("5' 색상:"+st.nextToken());
+//		System.out.println("6' 색상:"+st.nextToken());
+//		System.out.println("7' 색상:"+st.nextToken());
+//		for(int i=0;i<=st.countTokens();i++)
+//		{
+//			System.out.println(i+"' 색상:"+st.nextToken());
+//		}
+		int i=1;
+		while(st.hasMoreElements())
+		{
+			System.out.println(i+"' 색상:"+st.nextToken());
+			i++;
+		}
+	}
+
+}
